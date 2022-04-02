27984142e0bf612e38ec3b42e87c630ffa2c4c40
Michael Huang <michael.m.huang@oracle.com>
2022-04-02 16:06:40 -0700

    Add StoresPage

---
+++ src/StoresPage.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import React from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BiStore } from "react-icons/bi";
+import { MdOutlineInventory2, MdOutlineAttachMoney } from "react-icons/md";
+import { IoIosAirplane } from "react-icons/io";
+import { GiDeliveryDrone } from "react-icons/gi";
+import { BsFillPersonFill } from "react-icons/bs";
+
+function StoresPage() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Hello!
+    </div>
+  );
+}
+
+export default StoresPage;
