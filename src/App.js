bb6f697fe2f2c15446063381a0502921916c758f
Michael Huang <michael.m.huang@oracle.com>
2022-04-02 15:49:15 -0700

    Added home screen with buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BiStore } from "react-icons/bi";
+import { MdOutlineInventory2, MdOutlineAttachMoney } from "react-icons/md";
+import { IoIosAirplane } from "react-icons/io";
+import { GiDeliveryDrone } from "react-icons/gi";
+import { BsFillPersonFill } from "react-icons/bs";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to the Grocery Express Delivery Service!</p>
-        <p>Select a command to get started: </p>
-        <button>Make Store</button>
-        <button>Display Stores</button>
+        <p>Select a category to get started: </p>
+
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            <BiStore /> Stores
+          </Button>
+        </div>
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            <MdOutlineInventory2 /> Items
+          </Button>
+        </div>
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            <IoIosAirplane /> Pilots
+          </Button>
+        </div>
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            <GiDeliveryDrone /> Drones
+          </Button>
+        </div>
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            <BsFillPersonFill /> Customers
+          </Button>
+        </div>
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            <MdOutlineAttachMoney /> Orders
+          </Button>
+        </div>
       </header>
     </div>
   );
 }
-
+{
+}
 export default App;
