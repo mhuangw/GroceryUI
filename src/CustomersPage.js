823c72ede5e02e41b56f526f0dfd808c223e8e15
Michael Huang <michael.m.huang@oracle.com>
2022-04-03 10:38:15 -0700

    Added base template for rest of pages

---
+++ src/CustomersPage.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import React from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+
+function CustomersPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Link to="/StoresPage">
+            <Button className="buttonStyle" variant="secondary" size="lg">
+              Make Store
+            </Button>
+          </Link>
+        </div>
+        <div>
+          <Button className="buttonStyle" variant="secondary" size="lg">
+            Display Stores
+          </Button>
+        </div>
+        <div style={{ marginTop: "20px", fontSize: "large" }}>
+          <Link to="/">Back to Home</Link>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default CustomersPage;
