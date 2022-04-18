d35c40e7f5ef7c7d32f5c3ff8d21f60fc47232bc
Michael Huang <michael.m.huang@oracle.com>
2022-04-17 17:21:56 -0700

    Integrated make drone page

--- src/MakeDronePage.js
+++ src/MakeDronePage.js
@@ -1,25 +1,63 @@
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const url = "https://cs6310-14.azurewebsites.net/drone/make_drone";
 
 function MakeDronePage() {
+  const [storeName, setStoreName] = useState("");
+  const [id, setId] = useState("");
+  const [weightCapacity, setWeightCapacity] = useState(0);
+  const [tripsLeft, setTripsLeft] = useState(0);
+
+  const makeDrone = () => {
+    axios
+      .post(url, {
+        storeName: storeName,
+        id: id,
+        weightCapacity: weightCapacity,
+        trips_left: tripsLeft,
+      })
+      .then(() => {
+        alert("Drone added!");
+        document.location.reload();
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <Form>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Store Name" />
+              <Form.Control
+                placeholder="Store Name"
+                onChange={(e) => setStoreName(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Drone ID" />
+              <Form.Control
+                placeholder="Drone ID"
+                onChange={(e) => setId(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Weight Capacity" />
+              <Form.Control
+                placeholder="Weight Capacity"
+                onChange={(e) => setWeightCapacity(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Remaining Deliveries" />
+              <Form.Control
+                placeholder="Remaining Trips"
+                onChange={(e) => setTripsLeft(e.target.value)}
+              />
             </Form.Group>
-            <Button variant="secondary" type="submit">
+            <Button variant="secondary" onClick={() => makeDrone()}>
               Submit
             </Button>
             <div style={{ marginTop: "20px" }}>
