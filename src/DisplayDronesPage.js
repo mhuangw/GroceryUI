d9e095ebebdae6404edd707903cb43f30284fe43
Michael Huang <michael.m.huang@oracle.com>
2022-04-17 18:08:10 -0700

    Added display drones page

--- src/DisplayDronesPage.js
+++ src/DisplayDronesPage.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Table, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
