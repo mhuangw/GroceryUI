41be614d6b927c55a8ebe154b9ee5440f2a39ae3
Michael Huang <michael.m.huang@oracle.com>
2022-04-17 17:07:55 -0700

    Integrated make pilot page

--- src/MakePilotPage.js
+++ src/MakePilotPage.js
@@ -1,34 +1,87 @@
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const url = "https://cs6310-14.azurewebsites.net/pilot/make_pilot";
 
 function MakePilotPage() {
+  const [account, setAccount] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [taxId, setTaxId] = useState("");
+  const [licenseId, setLicenseId] = useState("");
+  const [experienceLevel, setExperienceLevel] = useState(0);
+
+  const makePilot = () => {
+    axios
+      .post(url, {
+        account: account,
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        taxId: taxId,
+        licenseId: licenseId,
+        experienceLevel: experienceLevel,
+      })
+      .then(() => {
+        alert("Pilot added!");
+        document.location.reload();
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <Form>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Account ID" />
+              <Form.Control
+                placeholder="Account ID"
+                onChange={(e) => setAccount(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="First Name" />
+              <Form.Control
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Last Name" />
+              <Form.Control
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Phone Number" />
+              <Form.Control
+                placeholder="Phone Number"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Tax ID" />
+              <Form.Control
+                placeholder="Tax ID"
+                onChange={(e) => setTaxId(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="License ID" />
+              <Form.Control
+                placeholder="License ID"
+                onChange={(e) => setLicenseId(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Experience Level" />
+              <Form.Control
+                placeholder="Experience Level"
+                onChange={(e) => setExperienceLevel(e.target.value)}
+              />
             </Form.Group>
-            <Button variant="secondary" type="submit">
+            <Button variant="secondary" onClick={() => makePilot()}>
               Submit
             </Button>
             <div style={{ marginTop: "20px" }}>
