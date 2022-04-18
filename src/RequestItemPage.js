7fe4b97da91001b0954d80806d731f30e377493e
Michael Huang <michael.m.huang@oracle.com>
2022-04-17 18:08:43 -0700

    Integrated request item page

--- src/RequestItemPage.js
+++ src/RequestItemPage.js
@@ -1,32 +1,75 @@
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
+const url = "https://cs6310-14.azurewebsites.net/order/request_item";
 
 function RequestItemPage() {
+  const [storeName, setStoreName] = useState("");
+  const [orderId, setOrderId] = useState("");
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  const requestItem = () => {
+    axios
+      .post(url, {
+        storeName: storeName,
+        orderId: orderId,
+        name: name,
+        quantity: quantity,
+        price: price,
+      })
+      .then(() => {
+        alert("Item requested!");
+        document.location.reload();
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <Form>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Store Name" />
+              <Form.Control
+                placeholder="Store Name"
+                onChange={(e) => setStoreName(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Order ID" />
+              <Form.Control
+                placeholder="Order ID"
+                onChange={(e) => setOrderId(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Item Name" />
+              <Form.Control
+                placeholder="Item Name"
+                onChange={(e) => setName(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Item Quantity" />
+              <Form.Control
+                placeholder="Item Quantity"
+                onChange={(e) => setQuantity(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control placeholder="Unit Price" />
+              <Form.Control
+                placeholder="Unit Price"
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </Form.Group>
-            <Button variant="secondary" type="submit">
+            <Button variant="secondary" onClick={() => requestItem()}>
               Submit
             </Button>
             <div style={{ marginTop: "20px" }}>
-              <Link to="/StoresPage">
+              <Link to="/OrdersPage">
                 <Button className="buttonStyle" variant="secondary" size="sm">
                   Go Back
                 </Button>
